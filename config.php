314d14e84d5a19ddc5682d81a590722205aeb160
Rizaloid <36554202+Rizaloid@users.noreply.github.com>
2018-10-28 07:11:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 //masukan config token yg terdapat pada link di packet captur
 // jangan hapus tanda kutip(" ") 
 
-$access_token = "6d8c3c99-b5a1-4b0b-972d-74602c116be4";
+$access_token = "c9f6602d-bfb3-40a5-8607-248ff84d764a";
 
 // kamu bisa membuat banyak config untuk beberapa akun.
